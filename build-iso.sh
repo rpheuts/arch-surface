2fc167bdb11c393efae82a9123b52629e3ae8170
Robert Heuts <rpheuts@gmail.com>
2015-12-30 19:47:00 -0800

    Added code for building iso using archiso (untested)

--- build-iso.sh
+++ build-iso.sh
@@ -32,6 +32,64 @@ else
 	cp ../configs/default/.config ./
 fi
 
+echo Configuring ${KERNEL} kernel...
 yes "" | make oldconfig
 
+echo Building ${KERNEL} kernel and modules...
 make -j2
+make modules_install
+
+popd
+
+mkdir archiso
+cp -r /usr/share/archiso/configs/baseline/* ./archiso/
+cp packages.x86_64 ./archiso/
+
+##### BUILD ISO ######
+
+pushd archiso
+
+set -e -u
+
+iso_name=archlinux
+iso_label="ARCH_$(date +%Y%m)"
+iso_version=$(date +%Y.%m.%d)
+install_dir=arch
+arch=$(uname -m)
+work_dir=work
+out_dir=out
+
+script_path=$(readlink -f ${0%/*})
+
+mkarchiso -v -w "${work_dir}" -D "${install_dir}" init
+
+cp ../linux-4.3.3/arch/x86_64/boot/bzImage work/airootfs/boot/vmlinuz-linux
+cp -r /lib/modules/* work/airootfs/lib/modules/
+
+mkdir -p ${work_dir}/airootfs/etc/initcpio/hooks
+mkdir -p ${work_dir}/airootfs/etc/initcpio/install
+cp /usr/lib/initcpio/hooks/archiso ${work_dir}/airootfs/etc/initcpio/hooks
+cp /usr/lib/initcpio/install/archiso ${work_dir}/airootfs/etc/initcpio/install
+cp ${script_path}/mkinitcpio.conf ${work_dir}/airootfs/etc/mkinitcpio-archiso.conf
+mkarchiso -v -w "${work_dir}" -D "${install_dir}" -r 'mkinitcpio -c /etc/mkinitcpio-archiso.conf -k /boot/vmlinuz$
+
+mkdir -p ${work_dir}/iso/${install_dir}/boot/${arch}
+cp ${work_dir}/airootfs/boot/archiso.img ${work_dir}/iso/${install_dir}/boot/${arch}/archiso.img
+cp ${work_dir}/airootfs/boot/vmlinuz-linux ${work_dir}/iso/${install_dir}/boot/${arch}/vmlinuz
+
+mkdir -p ${work_dir}/iso/${install_dir}/boot/syslinux
+sed "s|%ARCHISO_LABEL%|${iso_label}|g;
+     s|%INSTALL_DIR%|${install_dir}|g;
+     s|%ARCH%|${arch}|g" ${script_path}/syslinux/syslinux.cfg > ${work_dir}/iso/${install_dir}/boot/syslinux/sysl$
+cp ${work_dir}/airootfs/usr/lib/syslinux/bios/ldlinux.c32 ${work_dir}/iso/${install_dir}/boot/syslinux/
+cp ${work_dir}/airootfs/usr/lib/syslinux/bios/menu.c32 ${work_dir}/iso/${install_dir}/boot/syslinux/
+cp ${work_dir}/airootfs/usr/lib/syslinux/bios/libutil.c32 ${work_dir}/iso/${install_dir}/boot/syslinux/
+
+mkdir -p ${work_dir}/iso/isolinux
+sed "s|%INSTALL_DIR%|${install_dir}|g" ${script_path}/isolinux/isolinux.cfg > ${work_dir}/iso/isolinux/isolinux.c$
+cp ${work_dir}/airootfs/usr/lib/syslinux/bios/isolinux.bin ${work_dir}/iso/isolinux/
+cp ${work_dir}/airootfs/usr/lib/syslinux/bios/isohdpfx.bin ${work_dir}/iso/isolinux/
+cp ${work_dir}/airootfs/usr/lib/syslinux/bios/ldlinux.c32 ${work_dir}/iso/isolinux/
+
+mkarchiso -v -w "${work_dir}" -D "${install_dir}" prepare
+mkarchiso -v -w "${work_dir}" -D "${install_dir}" -L "${iso_label}" -o "${out_dir}" iso "${iso_name}-${iso_version}-${arch}.iso"
