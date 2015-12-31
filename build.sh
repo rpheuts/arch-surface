a28a2712625a90d32acde80abc527d6240422043
Robert Heuts <rpheuts@gmail.com>
2015-12-30 23:45:58 -0800

    Fixed a static path to a dynamic one

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 docker build --tag rpheuts/arch-surface .
-docker run --privileged -v /home/rpheuts/work/src/github.com/rpheuts/arch-surface:/work/ rpheuts/arch-surface
+docker run --privileged -v `pwd`:/work/ rpheuts/arch-surface
